4ca0e7ad8a18e7152e173548e8ad35f03616255a
Danut Popa <scorpionipx@gmail.com>
2017-10-24 17:53:26 +0300

    beggined to write asm script

--- LAB4/tema/asm/filtru.asm
+++ LAB4/tema/asm/filtru.asm
@@ -2,41 +2,120 @@
 
 ; Enunt: sa se scrie un program ASM in care sa se implementeze filtrul recurent
 ; definit de relatia: Yn = 1.2 * X0 - 0.9 * X1 - 0.7 Yn-1.
+
+; 	Pentru a nu folosi variabile in format cu virgula mobila, se inmultesc valorile 
+; valorile constante cu un factor putere a lui 2. Factorul este putere a lui 2 pentru
+; a se folosi instructiuni de shiftare in locul celor de impartire.
+
+; 1.2 * 32 = 38.4 ==> 38
+; 0.9 * 32 = 28.8 ==> 29                      S-a ales factorul 32 = 2 ^ 5
+; 0.7 * 32 = 22.4 ==> 22
+
 ; ScorpionIPX
 
+#define FACTOR D'32'
+#define FACTOR_POW D'5'
+
+#define CONSTANT_0 D'38'
+#define CONSTANT_1 D'29'
+#define CONSTANT_2 D'22'
+
+#define INIT_Y_L D'3'
+#define INIT_Y_H D'0'
+#define NUM_ITERATION D'5' 
+
 ;assign memory for variables
-n equ 0x20; last coef to be added
-s equ 0x21; calculated sum
-c equ 0x22; coef to be added
+x0 equ 0x20
+x1 equ 0x21
+
+y0_L equ 0x22
+y0_H equ 0x23
+
+y1_L equ 0x24
+y1_H equ 0x25
+
+i equ 0x26
+
+aux_L equ 0x27
+aux_H equ 0x28
+
 
 main:
 	; assign values to variables
-	MOVLW D'5'; W <- 9 - acumulator gets value 9
-	MOVWF n;n <- W, - x gets acumulator value
 
-	MOVLW D'0'; W <- 0 - acumulator gets value 0
-	MOVWF s;s <- W - c gets acumulator value
+	MOVLW D'15'; W <- 15
+	MOVWF x0; x0 <- 15
 
-	;MOVLW D'0'; W <- 0 - acumulator gets value 0
-	MOVWF c;c <- W - c gets acumulator value
-	NOP;
+	MOVLW D'7'; W <- 7
+	MOVWF x1; x1 <- 7
 
+	MOVLW INIT_Y_L; W <- INIT_Y_L
+	MOVWF y0_L; y0_H <- INIT_Y_L
 
-	;if (n - c >= 0) => carry = 1
-	DECF n; decrement n for easier implementation
-	calculate_sum:
-		MOVF c, 0; load coef into acumulator
-		SUBWF n, 0; W = n - c
-		BTFSS STATUS, C;check if carry; C = 0, position of carry flag bit
-		GOTO end_calculate_sum; if (n - c >= 0), sum is not calclated
-		INCF c; increment coeficient
-		NOP;
-		MOVF c, 0; load coef into acumulator
-		NOP;
-		ADDWF s, 1; add acumulator to sum's value
-		GOTO calculate_sum
-		NOP;
+	MOVLW INIT_Y_H; W <- INIT_Y_H
+	MOVWF y0_H; y0_H <- INIT_Y_H
+
 	
-	end_calculate_sum:
+	MOVLW D'0'; W <- 0
+	MOVWF y1_L; y1_L <- 0
+	MOVWF y1_H; y1_H <- 0
+	MOVWF aux_L; aux_L <- 0
+	MOVWF aux_H; aux_H <- 0
+
+	NOP
+
+	MOVLW NUM_ITERATION; W<- NUM_ITERATION
+	MOVWF i; i <- W
+
+	NOP
+
+	MOVF i, 1;
+	NOP
+	BTFSS i, Z; skip if i is non zero
+	GOTO END_
+
+	ITER_LOOP:
+
+		NOP
+
+		CALL CALC_Y
+
+		NOP
+
+		DECFSZ i; if i is zero, skip
+		GOTO ITER_LOOP
+
+		GOTO END_
+
+	
+	CALC_Y:
+		
+		MOVLW D'0'; W <- 0
+		MOVWF aux_L; aux_L <- 0
+		MOVWF aux_H; aux_H <- 0
+
+		MOVLW CONSTANT_0; W <- CONSTANT_0
+		NOP
+		MOVWF i; i <- W
+
+		MOVLW D'0'; W <- 0
+
+		NOP
+
+		CALC_T1:
+			ADDWF x0, 0; W <_ W + x0
+			MOVWF aux_L; aux_L = W
+			BTFSC STATUS, C; if overflow, increase auh_H
+			INCF aux_H;
+
+			NOP
+	
+			DECFSZ i
+			GOTO CALC_T1
+
+		RETURN
+	
+
+	END_:
 		NOP
 	end
