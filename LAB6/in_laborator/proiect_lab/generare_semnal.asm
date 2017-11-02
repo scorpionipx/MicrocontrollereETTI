cac827414185e625b8cf55144337fd5482a2baec
DESKTOP-PE8APUQ\ScorpionIPX <scorpionipx@gmail.com>
2017-11-02 11:31:34 +0200

    bullshit

--- LAB6/in_laborator/proiect_lab/generare_semnal.asm
+++ LAB6/in_laborator/proiect_lab/generare_semnal.asm
@@ -1,38 +1,32 @@
 #include p16f84.inc
 
-i equ 0x20; contor generare semnal
+i equ 0x20
 
 main:
 	BSF STATUS, RP0; pozitionare bank 1
-	MOVLW B'11111110'
+	MOVLW B'11111100'
 	MOVWF TRISB; RB0 output
 	BCF STATUS, RP0; revenire in bank 0
 	
 	MOVLW D'5'
 	MOVWF i
-	
-	LOOP:
-		BSF PORTB, 0; set 1 to RB0
-		NOP; keep 1 on RB0
-		NOP; keep 1 on RB0
-	
-		BCF PORTB, 0; set 0 to RB0
-		NOP; keep 0 on RB0
-		NOP; keep 0 on RB0
-		NOP; keep 0 on RB0
-	
-		BSF PORTB, 0; set 1 to RB0
-		NOP; keep 1 on RB0
-	
-		BCF PORTB, 0; set 0 to RB0
 
-		DECFSZ i, 1
-		GOTO LOOP
-
+	MOVLW B'00000001'
+	MOVWF PORTB
 	NOP
-	end
-
-
-	
+	LOOP:
+	BSF PORTB, 1
+	BCF PORTB, 0
+	CLRF PORTB
+	BSF PORTB, 1
+	NOP
+	COMF PORTB, 1
+	BSF PORTB, 1
+	BCF PORTB, 0
+	COMF PORTB
+	DECFSZ i,1 
+	GOTO LOOP
+NOP
+end
 
 	
