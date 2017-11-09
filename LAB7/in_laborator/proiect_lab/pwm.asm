ab192211df4d11bb3e38b2e2a28b00cb4d2aa2e4
DESKTOP-PE8APUQ\ScorpionIPX <scorpionipx@gmail.com>
2017-11-09 10:36:35 +0200

    forma unda generata

---
+++ LAB7/in_laborator/proiect_lab/pwm.asm
@@ -0,0 +1,50 @@
+; fosc = 4MHz
+; 1 cm = 4 tacte
+; 1 sec = 10^6 cm
+; 1 cm = 1 us
+; 1 ms = 1000 cm
+
+#include p16f84.inc
+
+i equ 0x20
+j equ 0x21
+
+main:
+	BSF STATUS, RP0
+	MOVLW B'00001111'
+	MOVWF TRISB
+	BCF STATUS, RP0
+	
+	MOVLW D'10'
+	MOVWF i
+
+LOOP_PWM:
+	BSF PORTB, 5
+	CALL delay_1ms
+	NOP 
+	NOP
+	NOP
+	NOP
+
+	BCF PORTB, 5
+	CALL delay_1ms
+	NOP
+
+	DECFSZ i, 1
+	GOTO LOOP_PWM
+	NOP
+	
+
+delay_1ms:
+	MOVLW D'198'
+	MOVWF j
+	
+	loop_j:
+		NOP
+		NOP
+		DECFSZ j, 1
+		GOTO loop_j
+		RETURN
+
+end
+	
